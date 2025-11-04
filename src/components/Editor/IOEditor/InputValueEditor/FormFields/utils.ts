7ebce1cf0b7e3c3931749b40397690655dbef1b6
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2025-11-04 10:44:00 -0800

    Misc Fixes

--- src/components/Editor/IOEditor/InputValueEditor/FormFields/utils.ts
+++ src/components/Editor/IOEditor/InputValueEditor/FormFields/utils.ts
@@ -2,14 +2,15 @@ import { type ComponentSpec } from "@/utils/componentSpec";
 
 export const checkNameCollision = (
   newName: string,
-  currentOutputName: string,
+  currentIOName: string,
   componentSpec: ComponentSpec,
   type: "inputs" | "outputs",
 ) => {
   if (!componentSpec[type]) return false;
 
-  // Check if any other output (not the current one) has the same name
   return componentSpec[type].some(
-    (output) => output.name === newName && output.name !== currentOutputName,
+    (IO) =>
+      IO.name.toLowerCase() === newName.toLowerCase() &&
+      IO.name.toLowerCase() !== currentIOName.toLowerCase(),
   );
 };
