Cloud-Pipelines/pipeline-studio-app

Migrate TaskStatusMap to PipelineRunProvider

src/types/pipelineRun.ts
@@ -1,3 +1,5 @@
+import type { ContainerExecutionStatus } from "@/api/types.gen";
+
 export interface PipelineRun {
   id: number;
   root_execution_id: number;
@@ -17,4 +19,10 @@ export interface TaskStatusCounts {
   waiting: number;
   skipped: number;
   cancelled: number;
+}
+
+export interface ExecutionStatus {
+  run: string;
+  map: Map<string, ContainerExecutionStatus>;
+  counts: TaskStatusCounts;
 }

TangleML/tangle-ui

src/types/pipelineRun.ts
@@ -1,3 +1,4 @@
+import type { ContainerExecutionStatus } from "@/api/types.gen";
 /**
  * Possible status values for a pipeline run, derived from aggregating task statuses
  */
@@ -28,4 +29,10 @@ export interface TaskStatusCounts {
   waiting: number;
   skipped: number;
   cancelled: number;
+}
+
+export interface ExecutionStatus {
+  run: RunStatus;
+  map: Record<string, ContainerExecutionStatus>;
+  counts: TaskStatusCounts;
 }