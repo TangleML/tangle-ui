Cloud-Pipelines/pipeline-studio-app

replace legacy library with separate classes

src/providers/ComponentLibraryProvider/libraries/browserPersistedLibrary.ts
@@ -1,0 +1,141 @@
+import { hydrateComponentReference } from "@/services/componentService";
+import type { ComponentFolder } from "@/types/componentLibrary";
+import { type ComponentReference, hasValidDigest } from "@/utils/componentSpec";
+
+import { isValidFilterRequest, type LibraryFilterRequest } from "../types";
+import { LibraryDB, type StoredLibrary } from "./storage";
+import {
+  DuplicateComponentError,
+  InvalidComponentReferenceError,
+  type Library,
+} from "./types";
+import { dispatchLibraryChangeEvent } from "./utils";
+
+class LibraryNotFoundError extends Error {
+  name = "LibraryNotFoundError";
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class BrowserPersistedLibrary implements Library {
+  #loading: Promise<void>;
+  #components = new Map<string, ComponentReference>();
+  #eventEmitter: EventTarget = new EventTarget();
+
+  constructor(
+    private readonly libId: string,
+    private readonly createLibrary: () => Promise<StoredLibrary | undefined>,
+  ) {
+    this.#loading = this.#loadLibrary();
+  }
+
+  get eventEmitter() {
+    return this.#eventEmitter;
+  }
+
+  async #loadLibrary() {
+    const library =
+      (await LibraryDB.component_libraries.get(this.libId)) ??
+      (await this.createLibrary());
+
+    if (!library) {
+      throw new LibraryNotFoundError(`Library ${this.libId} not found`);
+    }
+
+    const components = library.components ?? [];
+
+    const hydratedComponents = (
+      await Promise.all(
+        components.map(async (component) => {
+          return await hydrateComponentReference(component);
+        }),
+        // todo: handle per-component errors
+      )
+    ).filter((c) => hasValidDigest(c));
+
+    hydratedComponents.forEach((component) => {
+      this.#components.set(component.digest, component);
+    });
+  }
+
+  async hasComponent(component: ComponentReference): Promise<boolean> {
+    await this.#loading;
+
+    if (!hasValidDigest(component)) {
+      return false;
+    }
+
+    return this.#components.has(component.digest);
+  }
+
+  async addComponent(component: ComponentReference): Promise<void> {
+    await this.#loading;
+
+    const hydratedComponent = await hydrateComponentReference(component);
+
+    if (!hydratedComponent) {
+      throw new InvalidComponentReferenceError(component);
+    }
+
+    if (this.#components.has(hydratedComponent.digest)) {
+      throw new DuplicateComponentError(component);
+    }
+
+    await LibraryDB.component_libraries
+      .where("id")
+      .equals(this.libId)
+      .modify((lib) => {
+        lib.knownDigests = Array.from(
+          new Set([...(lib.knownDigests ?? []), hydratedComponent.digest]),
+        );
+        lib.components = [...(lib.components ?? []), hydratedComponent];
+      });
+
+    this.#components.set(hydratedComponent.digest, hydratedComponent);
+
+    dispatchLibraryChangeEvent(this, "add", hydratedComponent);
+  }
+
+  async removeComponent(component: ComponentReference): Promise<void> {
+    await this.#loading;
+
+    if (!hasValidDigest(component)) {
+      throw new InvalidComponentReferenceError(component);
+    }
+
+    // todo: handle options like what library/storage
+    await LibraryDB.component_libraries
+      .where("id")
+      .equals(this.libId)
+      .modify((lib) => {
+        lib.components = lib.components?.filter(
+          (c) => c.digest !== component.digest,
+        );
+        lib.knownDigests = lib.knownDigests?.filter(
+          (digest) => digest !== component.digest,
+        );
+      });
+
+    this.#components.delete(component.digest);
+
+    dispatchLibraryChangeEvent(this, "remove", component);
+  }
+
+  async getComponents(filter?: LibraryFilterRequest): Promise<ComponentFolder> {
+    await this.#loading;
+
+    if (isValidFilterRequest(filter)) {
+      // todo: filter components
+    }
+
+    const library = await LibraryDB.component_libraries.get(this.libId);
+
+    return {
+      name: library?.name ?? this.libId,
+      components: Array.from(this.#components.values()),
+      // todo: support folders
+      folders: [],
+    };
+  }
+}

src/providers/ComponentLibraryProvider/libraries/graphSpecReadonlyLibrary.ts
@@ -1,0 +1,112 @@
+import { hydrateComponentReference } from "@/services/componentService";
+import type { ComponentFolder } from "@/types/componentLibrary";
+import {
+  type ComponentReference,
+  type GraphSpec,
+  hasValidDigest,
+} from "@/utils/componentSpec";
+
+import { isValidFilterRequest, type LibraryFilterRequest } from "../types";
+import { type Library, ReadOnlyLibraryError } from "./types";
+import { dispatchLibraryChangeEvent } from "./utils";
+
+export class GraphSpecReadonlyLibrary implements Library {
+  #graphSpec: GraphSpec | undefined;
+  #components: Map<string, ComponentReference> = new Map();
+  #eventEmitter: EventTarget = new EventTarget();
+
+  constructor(
+    private readonly name: string,
+    graphSpec: GraphSpec | undefined,
+  ) {
+    void this.setGraphSpec(graphSpec);
+  }
+
+  get eventEmitter() {
+    return this.#eventEmitter;
+  }
+
+  async setGraphSpec(graphSpec: GraphSpec | undefined) {
+    this.#graphSpec = graphSpec;
+    void this.#refresh();
+  }
+
+  async getComponents(filter?: LibraryFilterRequest): Promise<ComponentFolder> {
+    if (isValidFilterRequest(filter)) {
+      const components = Array.from(this.#components.values()).filter(
+        (component) => {
+          // todo: search by some other fields from
+          return component.name
+            ?.toLowerCase()
+            .includes(filter.searchTerm.toLowerCase());
+        },
+      );
+
+      return {
+        name: this.name,
+        components,
+        folders: [],
+      };
+    }
+
+    return {
+      name: this.name,
+      components: Array.from(this.#components.values()),
+      folders: [],
+    };
+  }
+
+  async hasComponent(component: ComponentReference): Promise<boolean> {
+    await this.#refresh();
+
+    if (!hasValidDigest(component)) {
+      return false;
+    }
+
+    return this.#components.has(component.digest);
+  }
+
+  async addComponent(): Promise<void> {
+    throw new ReadOnlyLibraryError("This library is read-only.");
+  }
+
+  async removeComponent(): Promise<void> {
+    throw new ReadOnlyLibraryError("This library is read-only.");
+  }
+
+  async #refresh() {
+    this.#components.clear();
+
+    if (!this.#graphSpec) {
+      dispatchLibraryChangeEvent(this, "refresh");
+      return;
+    }
+
+    if (
+      !this.#graphSpec ||
+      !this.#graphSpec.tasks ||
+      typeof this.#graphSpec.tasks !== "object"
+    ) {
+      dispatchLibraryChangeEvent(this, "refresh");
+      return;
+    }
+
+    for (const task of Object.values(this.#graphSpec.tasks)) {
+      const key = task.componentRef.digest;
+      if (key) {
+        const hydratedComponent = await hydrateComponentReference(
+          task.componentRef,
+        );
+
+        if (!hydratedComponent) {
+          // todo: how to handle individual components that are failed to load?
+          return;
+        }
+
+        this.#components.set(key, hydratedComponent);
+      }
+    }
+
+    dispatchLibraryChangeEvent(this, "refresh");
+  }
+}

src/providers/ComponentLibraryProvider/libraries/yamlFileLibrary.ts
@@ -1,0 +1,154 @@
+import { hydrateComponentReference } from "@/services/componentService";
+import {
+  type ComponentFolder,
+  isValidComponentLibrary,
+} from "@/types/componentLibrary";
+import { loadObjectFromYamlData } from "@/utils/cache";
+import { type ComponentReference, hasValidDigest } from "@/utils/componentSpec";
+
+import { isValidFilterRequest, type LibraryFilterRequest } from "../types";
+import {
+  InvalidComponentReferenceError,
+  type Library,
+  ReadOnlyLibraryError,
+} from "./types";
+import { fetchWithCache } from "./utils";
+
+class YamlFileLibraryError extends Error {
+  name = "YamlFileLibraryError";
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class YamlFileLibrary implements Library {
+  #components: Map<string, ComponentReference> = new Map();
+  #rootFolder: ComponentFolder;
+  #loading: Promise<void>;
+
+  constructor(
+    name: string,
+    private readonly yamlFilePath: string,
+  ) {
+    this.#rootFolder = {
+      name,
+      folders: [],
+      components: [],
+    };
+
+    this.#loading = this.#loadLibrary();
+  }
+
+  async hasComponent(component: ComponentReference): Promise<boolean> {
+    await this.#loading;
+
+    if (!hasValidDigest(component)) {
+      throw new InvalidComponentReferenceError(component);
+    }
+
+    return this.#components.has(component.digest);
+  }
+
+  async addComponent(): Promise<void> {
+    throw new ReadOnlyLibraryError("This library is read-only");
+  }
+
+  async removeComponent(): Promise<void> {
+    throw new ReadOnlyLibraryError("This library is read-only");
+  }
+
+  async getComponents(filter?: LibraryFilterRequest): Promise<ComponentFolder> {
+    await this.#loading;
+
+    if (isValidFilterRequest(filter)) {
+      return {
+        name: this.#rootFolder.name,
+        // TODO: implement contentful filter
+        components: Array.from(this.#components.values()),
+      };
+    }
+
+    return {
+      name: this.#rootFolder.name,
+      components: this.#rootFolder.components ?? [],
+      folders: this.#rootFolder.folders ?? [],
+    };
+  }
+
+  async #loadLibrary() {
+    const response = await fetchWithCache(this.yamlFilePath);
+
+    if (!response.ok) {
+      throw new YamlFileLibraryError(
+        `Failed to fetch ${this.yamlFilePath}: ${response.status}`,
+      );
+    }
+
+    const data = await response.arrayBuffer();
+
+    const yamlData = loadObjectFromYamlData(data);
+
+    if (!isValidComponentLibrary(yamlData)) {
+      throw new YamlFileLibraryError(
+        `Invalid component library: ${this.yamlFilePath}`,
+      );
+    }
+
+    // visit all components and folders in the library
+    const queue = [
+      ...yamlData.folders.map((folder) => ({
+        folder,
+        parent: this.#rootFolder,
+      })),
+    ];
+
+    // BFS to process folders and components
+    while (queue.length > 0) {
+      const queuedFolder = queue.shift();
+      if (!queuedFolder) {
+        continue;
+      }
+
+      const { folder, parent } = queuedFolder;
+
+      const currentFolder: ComponentFolder = {
+        name: folder.name,
+        components: [],
+        folders: [],
+      };
+
+      for (const componentRef of folder.components ?? []) {
+        // todo: we can skip hydration if the component has at least digest, url and name
+        const hydratedComponentRef =
+          await hydrateComponentReference(componentRef);
+
+        if (!hydratedComponentRef) {
+          // todo: track component failure
+          console.warn(
+            `Failed to hydrate component: ${componentRef.url ?? componentRef.digest}`,
+            componentRef,
+          );
+          continue;
+        }
+
+        this.#components.set(hydratedComponentRef.digest, hydratedComponentRef);
+        currentFolder.components = [
+          ...(folder.components ?? []),
+          hydratedComponentRef,
+        ];
+      }
+
+      parent.folders = [...(parent.folders ?? []), currentFolder];
+
+      queue.push(
+        ...(folder.folders?.map((subfolder) => ({
+          folder: subfolder,
+          parent: currentFolder,
+        })) ?? []),
+      );
+    }
+
+    console.log("Loaded library", this.#rootFolder);
+  }
+}

TangleML/tangle-ui

src/providers/ComponentLibraryProvider/libraries/browserPersistedLibrary.ts
@@ -1,0 +1,144 @@
+import { hydrateComponentReference } from "@/services/componentService";
+import type { ComponentFolder } from "@/types/componentLibrary";
+import {
+  type ComponentReference,
+  isDiscoverableComponentReference,
+} from "@/utils/componentSpec";
+
+import { isValidFilterRequest, type LibraryFilterRequest } from "../types";
+import { LibraryDB, type StoredLibrary } from "./storage";
+import {
+  DuplicateComponentError,
+  InvalidComponentReferenceError,
+  type Library,
+} from "./types";
+import { dispatchLibraryChangeEvent } from "./utils";
+
+class LibraryNotFoundError extends Error {
+  name = "LibraryNotFoundError";
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class BrowserPersistedLibrary implements Library {
+  #loading: Promise<void>;
+  #components = new Map<string, ComponentReference>();
+  #eventEmitter: EventTarget = new EventTarget();
+
+  constructor(
+    private readonly libId: string,
+    private readonly createLibrary: () => Promise<StoredLibrary | undefined>,
+  ) {
+    this.#loading = this.#loadLibrary();
+  }
+
+  get eventEmitter() {
+    return this.#eventEmitter;
+  }
+
+  async #loadLibrary() {
+    const library =
+      (await LibraryDB.component_libraries.get(this.libId)) ??
+      (await this.createLibrary());
+
+    if (!library) {
+      throw new LibraryNotFoundError(`Library ${this.libId} not found`);
+    }
+
+    const components = library.components ?? [];
+
+    const hydratedComponents = (
+      await Promise.all(
+        components.map(async (component) => {
+          return await hydrateComponentReference(component);
+        }),
+        // todo: handle per-component errors
+      )
+    ).filter((c) => isDiscoverableComponentReference(c));
+
+    hydratedComponents.forEach((component) => {
+      this.#components.set(component.digest, component);
+    });
+  }
+
+  async hasComponent(component: ComponentReference): Promise<boolean> {
+    await this.#loading;
+
+    if (!isDiscoverableComponentReference(component)) {
+      return false;
+    }
+
+    return this.#components.has(component.digest);
+  }
+
+  async addComponent(component: ComponentReference): Promise<void> {
+    await this.#loading;
+
+    const hydratedComponent = await hydrateComponentReference(component);
+
+    if (!hydratedComponent) {
+      throw new InvalidComponentReferenceError(component);
+    }
+
+    if (this.#components.has(hydratedComponent.digest)) {
+      throw new DuplicateComponentError(component);
+    }
+
+    await LibraryDB.component_libraries
+      .where("id")
+      .equals(this.libId)
+      .modify((lib) => {
+        lib.knownDigests = Array.from(
+          new Set([...(lib.knownDigests ?? []), hydratedComponent.digest]),
+        );
+        lib.components = [...(lib.components ?? []), hydratedComponent];
+      });
+
+    this.#components.set(hydratedComponent.digest, hydratedComponent);
+
+    dispatchLibraryChangeEvent(this, "add", hydratedComponent);
+  }
+
+  async removeComponent(component: ComponentReference): Promise<void> {
+    await this.#loading;
+
+    if (!isDiscoverableComponentReference(component)) {
+      throw new InvalidComponentReferenceError(component);
+    }
+
+    // todo: handle options like what library/storage
+    await LibraryDB.component_libraries
+      .where("id")
+      .equals(this.libId)
+      .modify((lib) => {
+        lib.components = lib.components?.filter(
+          (c) => c.digest !== component.digest,
+        );
+        lib.knownDigests = lib.knownDigests?.filter(
+          (digest) => digest !== component.digest,
+        );
+      });
+
+    this.#components.delete(component.digest);
+
+    dispatchLibraryChangeEvent(this, "remove", component);
+  }
+
+  async getComponents(filter?: LibraryFilterRequest): Promise<ComponentFolder> {
+    await this.#loading;
+
+    if (isValidFilterRequest(filter)) {
+      // todo: filter components
+    }
+
+    const library = await LibraryDB.component_libraries.get(this.libId);
+
+    return {
+      name: library?.name ?? this.libId,
+      components: Array.from(this.#components.values()),
+      // todo: support folders
+      folders: [],
+    };
+  }
+}

src/providers/ComponentLibraryProvider/libraries/graphSpecReadonlyLibrary.ts
@@ -1,0 +1,112 @@
+import { hydrateComponentReference } from "@/services/componentService";
+import type { ComponentFolder } from "@/types/componentLibrary";
+import {
+  type ComponentReference,
+  type GraphSpec,
+  isDiscoverableComponentReference,
+} from "@/utils/componentSpec";
+
+import { isValidFilterRequest, type LibraryFilterRequest } from "../types";
+import { type Library, ReadOnlyLibraryError } from "./types";
+import { dispatchLibraryChangeEvent } from "./utils";
+
+export class GraphSpecReadonlyLibrary implements Library {
+  #graphSpec: GraphSpec | undefined;
+  #components: Map<string, ComponentReference> = new Map();
+  #eventEmitter: EventTarget = new EventTarget();
+
+  constructor(
+    private readonly name: string,
+    graphSpec: GraphSpec | undefined,
+  ) {
+    void this.setGraphSpec(graphSpec);
+  }
+
+  get eventEmitter() {
+    return this.#eventEmitter;
+  }
+
+  async setGraphSpec(graphSpec: GraphSpec | undefined) {
+    this.#graphSpec = graphSpec;
+    void this.#refresh();
+  }
+
+  async getComponents(filter?: LibraryFilterRequest): Promise<ComponentFolder> {
+    if (isValidFilterRequest(filter)) {
+      const components = Array.from(this.#components.values()).filter(
+        (component) => {
+          // todo: search by some other fields from
+          return component.name
+            ?.toLowerCase()
+            .includes(filter.searchTerm.toLowerCase());
+        },
+      );
+
+      return {
+        name: this.name,
+        components,
+        folders: [],
+      };
+    }
+
+    return {
+      name: this.name,
+      components: Array.from(this.#components.values()),
+      folders: [],
+    };
+  }
+
+  async hasComponent(component: ComponentReference): Promise<boolean> {
+    await this.#refresh();
+
+    if (!isDiscoverableComponentReference(component)) {
+      return false;
+    }
+
+    return this.#components.has(component.digest);
+  }
+
+  async addComponent(): Promise<void> {
+    throw new ReadOnlyLibraryError("This library is read-only.");
+  }
+
+  async removeComponent(): Promise<void> {
+    throw new ReadOnlyLibraryError("This library is read-only.");
+  }
+
+  async #refresh() {
+    this.#components.clear();
+
+    if (!this.#graphSpec) {
+      dispatchLibraryChangeEvent(this, "refresh");
+      return;
+    }
+
+    if (
+      !this.#graphSpec ||
+      !this.#graphSpec.tasks ||
+      typeof this.#graphSpec.tasks !== "object"
+    ) {
+      dispatchLibraryChangeEvent(this, "refresh");
+      return;
+    }
+
+    for (const task of Object.values(this.#graphSpec.tasks)) {
+      const key = task.componentRef.digest;
+      if (key) {
+        const hydratedComponent = await hydrateComponentReference(
+          task.componentRef,
+        );
+
+        if (!hydratedComponent) {
+          // todo: how to handle individual components that are failed to load?
+          return;
+        }
+
+        this.#components.set(key, hydratedComponent);
+      }
+    }
+
+    dispatchLibraryChangeEvent(this, "refresh");
+  }
+}

src/providers/ComponentLibraryProvider/libraries/yamlFileLibrary.ts
@@ -1,0 +1,157 @@
+import { hydrateComponentReference } from "@/services/componentService";
+import {
+  type ComponentFolder,
+  isValidComponentLibrary,
+} from "@/types/componentLibrary";
+import { loadObjectFromYamlData } from "@/utils/cache";
+import {
+  type ComponentReference,
+  isDiscoverableComponentReference,
+} from "@/utils/componentSpec";
+
+import { isValidFilterRequest, type LibraryFilterRequest } from "../types";
+import {
+  InvalidComponentReferenceError,
+  type Library,
+  ReadOnlyLibraryError,
+} from "./types";
+import { fetchWithCache } from "./utils";
+
+class YamlFileLibraryError extends Error {
+  name = "YamlFileLibraryError";
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class YamlFileLibrary implements Library {
+  #components: Map<string, ComponentReference> = new Map();
+  #rootFolder: ComponentFolder;
+  #loading: Promise<void>;
+
+  constructor(
+    name: string,
+    private readonly yamlFilePath: string,
+  ) {
+    this.#rootFolder = {
+      name,
+      folders: [],
+      components: [],
+    };
+
+    this.#loading = this.#loadLibrary();
+  }
+
+  async hasComponent(component: ComponentReference): Promise<boolean> {
+    await this.#loading;
+
+    if (!isDiscoverableComponentReference(component)) {
+      throw new InvalidComponentReferenceError(component);
+    }
+
+    return this.#components.has(component.digest);
+  }
+
+  async addComponent(): Promise<void> {
+    throw new ReadOnlyLibraryError("This library is read-only");
+  }
+
+  async removeComponent(): Promise<void> {
+    throw new ReadOnlyLibraryError("This library is read-only");
+  }
+
+  async getComponents(filter?: LibraryFilterRequest): Promise<ComponentFolder> {
+    await this.#loading;
+
+    if (isValidFilterRequest(filter)) {
+      return {
+        name: this.#rootFolder.name,
+        // TODO: implement contentful filter
+        components: Array.from(this.#components.values()),
+      };
+    }
+
+    return {
+      name: this.#rootFolder.name,
+      components: this.#rootFolder.components ?? [],
+      folders: this.#rootFolder.folders ?? [],
+    };
+  }
+
+  async #loadLibrary() {
+    const response = await fetchWithCache(this.yamlFilePath);
+
+    if (!response.ok) {
+      throw new YamlFileLibraryError(
+        `Failed to fetch ${this.yamlFilePath}: ${response.status}`,
+      );
+    }
+
+    const data = await response.arrayBuffer();
+
+    const yamlData = loadObjectFromYamlData(data);
+
+    if (!isValidComponentLibrary(yamlData)) {
+      throw new YamlFileLibraryError(
+        `Invalid component library: ${this.yamlFilePath}`,
+      );
+    }
+
+    // visit all components and folders in the library
+    const queue = [
+      ...yamlData.folders.map((folder) => ({
+        folder,
+        parent: this.#rootFolder,
+      })),
+    ];
+
+    // BFS to process folders and components
+    while (queue.length > 0) {
+      const queuedFolder = queue.shift();
+      if (!queuedFolder) {
+        continue;
+      }
+
+      const { folder, parent } = queuedFolder;
+
+      const currentFolder: ComponentFolder = {
+        name: folder.name,
+        components: [],
+        folders: [],
+      };
+
+      for (const componentRef of folder.components ?? []) {
+        // todo: we can skip hydration if the component has at least digest, url and name
+        const hydratedComponentRef =
+          await hydrateComponentReference(componentRef);
+
+        if (!hydratedComponentRef) {
+          // todo: track component failure
+          console.warn(
+            `Failed to hydrate component: ${componentRef.url ?? componentRef.digest}`,
+            componentRef,
+          );
+          continue;
+        }
+
+        this.#components.set(hydratedComponentRef.digest, hydratedComponentRef);
+        currentFolder.components = [
+          ...(folder.components ?? []),
+          hydratedComponentRef,
+        ];
+      }
+
+      parent.folders = [...(parent.folders ?? []), currentFolder];
+
+      queue.push(
+        ...(folder.folders?.map((subfolder) => ({
+          folder: subfolder,
+          parent: currentFolder,
+        })) ?? []),
+      );
+    }
+
+    console.log("Loaded library", this.#rootFolder);
+  }
+}