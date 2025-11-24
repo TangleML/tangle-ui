113a5b39808404c46229952ebbe8a3ea5c9809d9
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2025-11-24 15:38:21 -0800

    Remove Remaining Instances on Inline Licensing

--- src/appSettings.ts
+++ src/appSettings.ts
@@ -1,11 +1,3 @@
-/**
- * @license
- * Copyright 2022 Alexey Volkov
- * SPDX-License-Identifier: Apache-2.0
- * @author         Alexey Volkov <alexey.volkov+oss@ark-kun.com>
- * @copyright 2022 Alexey Volkov <alexey.volkov+oss@ark-kun.com>
- */
-
 import { BASE_URL } from "./utils/constants";
 
 // Settings: Default values and local storage configuration keys
