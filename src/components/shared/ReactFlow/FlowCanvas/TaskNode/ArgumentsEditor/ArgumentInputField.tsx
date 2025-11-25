d6a788c2aedb1df734dd7796bcad55b2a8e7b2e7
Camiel vs <93798047+camielvs@users.noreply.github.com>
2025-11-25 10:30:23 -0800

    Remove Remaining Instances of Inline Licensing (#1420)
    ## Description
    
    <!-- Please provide a brief description of the changes made in this pull request. Include any relevant context or reasoning for the changes. -->
    We're not displaying these licensing things in all of our files. We will have a master licence for the repo to cover everything.
    
    ## Related Issue and Pull requests
    
    <!-- Link to any related issues using the format #<issue-number> -->
    
    ## Type of Change
    
    <!-- Please delete options that are not relevant -->
    
    - [x] Documentation update
    
    ## Checklist
    
    <!-- Please ensure the following are completed before submitting the PR -->
    
    - [ ] I have tested this does not break current pipelines / runs functionality
    - [ ] I have tested the changes on staging
    
    ## Screenshots (if applicable)
    
    <!-- Include any screenshots that might help explain the changes or provide visual context -->
    
    ## Test Instructions
    
    <!-- Detail steps and prerequisites for testing the changes in this PR -->
    
    ## Additional Comments
    
    <!-- Add any additional context or information that reviewers might need to know regarding this PR -->

--- src/components/shared/ReactFlow/FlowCanvas/TaskNode/ArgumentsEditor/ArgumentInputField.tsx
+++ src/components/shared/ReactFlow/FlowCanvas/TaskNode/ArgumentsEditor/ArgumentInputField.tsx
@@ -1,11 +1,3 @@
-/**
- * @license
- * Copyright 2021 Alexey Volkov
- * SPDX-License-Identifier: Apache-2.0
- * @author         Alexey Volkov <alexey.volkov+oss@ark-kun.com>
- * @copyright 2021 Alexey Volkov <alexey.volkov+oss@ark-kun.com>
- */
-
 import equal from "fast-deep-equal";
 import { HelpCircle, Info, Maximize2, PlusSquare } from "lucide-react";
 import {
