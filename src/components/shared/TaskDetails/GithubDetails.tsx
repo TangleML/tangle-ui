a611773d5bf7800d8a643cea2d1b43bac00c1fdf
Volv G <volv.grebennikov@shopify.com>
2026-01-08 12:25:00 -0800

    feat: Add documentation URL to GithubDetails

--- src/components/shared/TaskDetails/GithubDetails.tsx
+++ src/components/shared/TaskDetails/GithubDetails.tsx
@@ -14,44 +14,73 @@ const linkProps = {
 export function GithubDetails({
   url,
   canonicalUrl,
+  documentationUrl,
   className,
 }: {
   url?: string;
   canonicalUrl?: string;
+  documentationUrl?: string;
   className?: string;
 }) {
-  if (!url && !canonicalUrl) return null;
+  const hasUrl = url || canonicalUrl;
+
+  if (!hasUrl && !documentationUrl) return null;
 
   return (
-    <BlockStack className={className}>
-      <Heading level={3}>URL</Heading>
-      {url && (
-        <>
-          <Link href={url} {...linkProps}>
-            View raw component.yaml
+    <BlockStack className={className} gap="2">
+      {!!hasUrl && (
+        <BlockStack>
+          <Heading level={3}>URL</Heading>
+          {!!url && (
+            <>
+              <Link href={url} {...linkProps}>
+                View raw component.yaml
+              </Link>
+
+              <Link
+                href={
+                  isGithubUrl(url) ? convertGithubUrlToDirectoryUrl(url) : url
+                }
+                {...linkProps}
+              >
+                View directory on GitHub
+              </Link>
+            </>
+          )}
+          {!!canonicalUrl && (
+            <>
+              <Link href={canonicalUrl} {...linkProps}>
+                View canonical URL
+              </Link>
+
+              <Link
+                href={convertGithubUrlToDirectoryUrl(canonicalUrl)}
+                {...linkProps}
+              >
+                View canonical URL on GitHub
+              </Link>
+            </>
+          )}
+        </BlockStack>
+      )}
+      {!!documentationUrl && (
+        <BlockStack>
+          <Heading level={3}>Documentation</Heading>
+          <Link href={documentationUrl} {...linkProps}>
+            View documentation
           </Link>
 
           <Link
-            href={isGithubUrl(url) ? convertGithubUrlToDirectoryUrl(url) : url}
+            href={
+              isGithubUrl(documentationUrl)
+                ? convertGithubUrlToDirectoryUrl(documentationUrl)
+                : documentationUrl
+            }
             {...linkProps}
           >
             View directory on GitHub
           </Link>
-        </>
-      )}
-      {canonicalUrl && (
-        <>
-          <Link href={canonicalUrl} {...linkProps}>
-            View canonical URL
-          </Link>
-
-          <Link
-            href={convertGithubUrlToDirectoryUrl(canonicalUrl)}
-            {...linkProps}
-          >
-            View canonical URL on GitHub
-          </Link>
-        </>
+        </BlockStack>
       )}
     </BlockStack>
   );
