c19eee9bf4fd9fbc83d18a75ecba0f14962f17d1
Morgan Wowk <morgan.wowk@shopify.com>
2026-01-15 10:10:56 -0500

    Make ConfigInput React compiler compatible

--- react-compiler.config.js
+++ react-compiler.config.js
@@ -28,6 +28,7 @@ export const REACT_COMPILER_ENABLED_DIRS = [
   "src/components/shared/GitHubLibrary",
 
   "src/components/shared/Submitters/Oasis/components",
+  "src/components/shared/Submitters/GoogleCloud/ConfigInput.tsx",
 
   // 11-20 useCallback/useMemo
   // "src/components/ui",                         // 12
