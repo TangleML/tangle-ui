54f47724673d14a3c6e2a88fd037338bd89a2006
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2026-01-21 17:13:43 -0800

    feat: Unpacked Subgraphs Retain External Connections

--- src/components/shared/ReactFlow/FlowCanvas/utils/replaceTaskArgumentsInGraphSpec.ts
+++ src/components/shared/ReactFlow/FlowCanvas/utils/replaceTaskArgumentsInGraphSpec.ts
@@ -1,20 +1,23 @@
 import type { ArgumentType, GraphSpec } from "@/utils/componentSpec";
+import { deepClone } from "@/utils/deepClone";
 
 const replaceTaskArgumentsInGraphSpec = (
   taskId: string,
   graphSpec: GraphSpec,
   taskArguments?: Record<string, ArgumentType>,
 ) => {
+  const cleanGraphSpec = deepClone(graphSpec);
+
   if (!taskArguments) {
     return graphSpec;
   }
 
   const newGraphSpec: GraphSpec = {
-    ...graphSpec,
+    ...cleanGraphSpec,
     tasks: {
-      ...graphSpec.tasks,
+      ...cleanGraphSpec.tasks,
       [taskId]: {
-        ...graphSpec.tasks[taskId],
+        ...cleanGraphSpec.tasks[taskId],
         arguments: taskArguments,
       },
     },
