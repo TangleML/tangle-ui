45d7160efacb9329777c15abc7f90077aac898fc
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2026-02-11 16:41:55 -0800

    hackdays: Add Buildings from Sidebar

--- src/components/Factory/Sidebar/Buildings.tsx
+++ src/components/Factory/Sidebar/Buildings.tsx
@@ -1,5 +1,20 @@
+import { BlockStack } from "@/components/ui/layout";
+import { Text } from "@/components/ui/typography";
+
+import { BUILDINGS } from "../data/buildings";
+import BuildingItem from "./BuildingItem";
+
 const Buildings = () => {
-  return <p>Building List</p>;
+  return (
+    <BlockStack gap="2">
+      <Text>Buildings</Text>
+      <BlockStack gap="1">
+        {BUILDINGS.map((building) => (
+          <BuildingItem key={building.id} building={building} />
+        ))}
+      </BlockStack>
+    </BlockStack>
+  );
 };
 
 export default Buildings;
