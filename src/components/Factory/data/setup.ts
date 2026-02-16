23e3f451290c1a9f17b4a56ec74c0fcf0519ae5a
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2026-02-15 22:42:01 -0800

    hackdays: Add Food Consumption and Game Over Screen

--- src/components/Factory/data/setup.ts
+++ src/components/Factory/data/setup.ts
@@ -1,19 +1,33 @@
 import type { XYPosition } from "@xyflow/react";
 
 import type { BuildingType } from "../types/buildings";
+import type { GlobalResourceType } from "./resources";
 
 type BuildingSetup = {
   type: BuildingType;
   position: XYPosition;
 };
+
+type ResourceSetup = {
+  type: GlobalResourceType;
+  amount: number;
+};
+
 interface setup {
   buildings?: BuildingSetup[];
+  resources?: ResourceSetup[];
 }
 
 const buildings: BuildingSetup[] = [
   { type: "firepit", position: { x: 0, y: 0 } },
 ];
 
+const resources: ResourceSetup[] = [
+  { type: "money", amount: 1000 },
+  { type: "food", amount: 200 },
+];
+
 export const setup: setup = {
   buildings,
+  resources,
 };
