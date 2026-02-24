b0bcef5d0d01ed375222f0ac5a5155ae40b3323d
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2026-02-23 17:05:04 -0800

    feat: Autolayout

--- src/components/Editor/PipelineEditor.tsx
+++ src/components/Editor/PipelineEditor.tsx
@@ -1,7 +1,7 @@
 import "@/styles/editor.css";
 
 import { Background, MiniMap, type ReactFlowProps } from "@xyflow/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { CollapsibleContextPanel } from "@/components/shared/ContextPanel/CollapsibleContextPanel";
 import {
@@ -21,12 +21,16 @@ import {
 import { ContextPanelProvider } from "@/providers/ContextPanelProvider";
 
 import { LoadingScreen } from "../shared/LoadingScreen";
+import type { FlowCanvasRef } from "../shared/ReactFlow/FlowCanvas/FlowCanvas";
+import type { LayoutAlgorithm } from "../shared/ReactFlow/FlowCanvas/utils/autolayout";
 import { NodesOverlayProvider } from "../shared/ReactFlow/NodesOverlay/NodesOverlayProvider";
 import PipelineDetails from "./Context/PipelineDetails";
 
 const GRID_SIZE = 10;
 
 const PipelineEditor = () => {
+  const flowCanvasRef = useRef<FlowCanvasRef>(null);
+
   const { componentSpec, isLoading } = useComponentSpec();
 
   const [flowConfig, setFlowConfig] = useState<ReactFlowProps>({
@@ -44,6 +48,10 @@ const PipelineEditor = () => {
     }));
   };
 
+  const handleAutoLayout = (algorithm: LayoutAlgorithm) => {
+    flowCanvasRef.current?.autoLayout(algorithm);
+  };
+
   // If the pipeline is loading or the component spec is empty, show a loading spinner
   if (isLoading || componentSpec === EMPTY_GRAPH_COMPONENT_SPEC) {
     return <LoadingScreen message="Loading Pipeline" />;
@@ -58,12 +66,13 @@ const PipelineEditor = () => {
               <InlineStack fill>
                 <FlowSidebar />
                 <BlockStack fill className="flex-1 relative">
-                  <FlowCanvas {...flowConfig}>
+                  <FlowCanvas ref={flowCanvasRef} {...flowConfig}>
                     <MiniMap position="bottom-left" pannable />
                     <FlowControls
-                      className="ml-56! mb-6!"
+                      className="ml-56! mb-3!"
                       config={flowConfig}
                       updateConfig={updateFlowConfig}
+                      onAutoLayout={handleAutoLayout}
                       showInteractive={false}
                     />
                     <Background gap={GRID_SIZE} className="bg-slate-50!" />
