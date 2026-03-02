4243c08d30b552e0173ec62a5ba73d54d0835081
mbeaulne <matt.beaulne@gmail.com>
2026-03-02 10:32:07 -0500

    Pipeline filters: content highlight

--- react-compiler.config.js
+++ react-compiler.config.js
@@ -51,6 +51,7 @@ export const REACT_COMPILER_ENABLED_DIRS = [
   "src/components/shared/ReactFlow/FlowControls/StackingControls.tsx",
   "src/components/shared/ReactFlow/FlowCanvas/FlexNode",
   "src/components/shared/ReactFlow/FlowCanvas/TaskNode/ArgumentsEditor/DynamicDataDropdown.tsx",
+  "src/components/shared/HighlightText.tsx",
 
   // 11-20 useCallback/useMemo
   // "src/components/ui",                         // 12
