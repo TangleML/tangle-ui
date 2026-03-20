4546903b573057e4a87f65903830a27ba8c7ec79
Maksym Yezhov <maxim.ezhov@shopify.com>
2026-03-19 23:08:36 -0700

    - feat: support run status indicator

--- src/routes/v2/pages/RunView/nodes/TaskNode/taskNode.manifest.ts
+++ src/routes/v2/pages/RunView/nodes/TaskNode/taskNode.manifest.ts
@@ -2,8 +2,12 @@ import { RunViewTaskDetails } from "@/routes/v2/pages/RunView/components/RunView
 import { taskManifestBase } from "@/routes/v2/shared/nodes/TaskNode/taskManifestBase";
 import type { NodeTypeManifest } from "@/routes/v2/shared/nodes/types";
 
+import { RunViewTaskNode } from "./RunViewTaskNode";
+
 export const taskManifest: NodeTypeManifest = {
   ...taskManifestBase,
+
+  component: RunViewTaskNode,
 
   updatePosition(_undo, _spec, _nodeId, _position) {},
   deleteNode(_undo, _spec, _nodeId) {},
