d635d90ba5f373c617455a14dac13350e26ebd92
Morgan Wowk <morgan.wowk@shopify.com>
2026-04-14 14:55:43 -0700

    Add analytics provider with session tracking and tests

--- src/hooks/useUserDetails.ts
+++ src/hooks/useUserDetails.ts
@@ -1,11 +1,13 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
 
 import { getUserDetails } from "../utils/user";
 
+export const userQueryOptions = queryOptions({
+  queryKey: ["user"],
+  staleTime: 1000 * 60 * 60 * 0.5, // 30 minutes
+  queryFn: getUserDetails,
+});
+
 export function useUserDetails() {
-  return useSuspenseQuery({
-    queryKey: ["user"],
-    staleTime: 1000 * 60 * 60 * 0.5, // 30 minutes
-    queryFn: () => getUserDetails(),
-  });
+  return useSuspenseQuery(userQueryOptions);
 }
