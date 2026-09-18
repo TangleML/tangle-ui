0da3c3263c9e74767667bc0b7568b6f85dc01058
Maksym Yezhov <maxim.ezhov@shopify.com>
2026-09-18 00:52:09 -0700

    feat: tangent - debug in tangent sequence

--- src/utils/componentSpec.ts
+++ src/utils/componentSpec.ts
@@ -493,13 +493,11 @@ export const isGraphInputArgument = (
   typeof arg === "object" && arg !== null && "graphInput" in arg;
 
 export const isDynamicDataArgument = (
-  arg?: ArgumentType,
+  arg?: unknown,
 ): arg is DynamicDataArgument =>
   typeof arg === "object" && arg !== null && "dynamicData" in arg;
 
-export const isSecretArgument = (
-  arg?: ArgumentType,
-): arg is DynamicDataArgument =>
+export const isSecretArgument = (arg?: unknown): arg is DynamicDataArgument =>
   isDynamicDataArgument(arg) &&
   typeof arg.dynamicData === "object" &&
   arg.dynamicData !== null &&
